ce4aef771311e4fb90b77a6e96536f4f073069ab
rockers ragu <rockers ragu@ROCKERS-RAGU.local.lan>
2014-08-29 20:43:04 -0700

    Updated!

---
+++ Irctcweek2/src/Modules/Paypal/com/Paypalregister.java
@@ -0,0 +1,23 @@
+package Modules.Paypal.com;
+
+import org.openqa.selenium.WebDriver;
+import Modules.WrapperMethods.CommonWrapperMethods;
+
+public class Paypalregister {
+
+	public static void main(String[] args) {
+	
+		WebDriver driver = null;
+		
+		CommonWrapperMethods cwm = new CommonWrapperMethods(driver);
+		
+		cwm.InvokeApp("https://developer.paypal.com/","ch");
+		
+		cwm.clickbylinkText("Sign Up");
+	
+		cwm.setvalueById("emailAddress", "test@testleaf.com");
+	}
+	
+	}
+
+
